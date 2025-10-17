7f9cd47baee508fda480dc47410bf4c9c103f063
Maruthi Praveen <maruthipraveenobilisetti@gmail.com>
2025-10-17 21:51:06 +0530

    Updated FrontEnd with new paths

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const mysql = require('mysql2');
-
+require('dotenv').config();
 const app=express();
 
 app.use(cors())
@@ -16,6 +16,12 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// const db = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'2004',
+//     database:'todo'
+// })
 
 db.connect((err)=>{
     if(err){
@@ -119,3 +125,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+
